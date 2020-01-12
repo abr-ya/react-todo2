fe2bef6e6e61930d1153e5ccce3d6825e569a0d0
Yaroslav <gamer05@list.ru>
2020-01-12 15:49:07 +0300

    Стили, навбар, контейнер.

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar: React.FC = (props) => {
+    console.log(props);
+    
+    return (
+        <nav>
+            <div className="nav-wrapper blue lighten-3 plr10">
+                <a href="/" className="brand-logo">React + Typescrypt + Materialize CSS</a>
+                <ul className="right hide-on-med-and-down">
+                    <li><a href="/">Список дел</a></li>
+                    <li><a href="/">About</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
